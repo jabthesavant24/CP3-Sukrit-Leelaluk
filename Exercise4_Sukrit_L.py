6233a894a65fe65fd497cb6e96bae43ffd1f0168
jabthesavant24 <84028633+jabthesavant24@users.noreply.github.com>
2021-07-24 17:32:12 +0900

    Add files via upload

---
+++ Exercise4_Sukrit_L.py
@@ -0,0 +1,10 @@
+ScoreFounEng = 80.5
+ScoreGeneBusiness = 65.5
+ScoreIntroComSystem = 93.0
+ScoreComPro = 77.0
+
+print("--- Your Score ---")
+print("Foundation English : ", ScoreFounEng)
+print("General Business : ", ScoreGeneBusiness)
+print("Introduction to Computer Systems : ", ScoreIntroComSystem)
+print("Computer Programming : ", ScoreComPro)
