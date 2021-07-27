1511157a9e52528a2e716a8e0cf8ae376b78eaae
jabthesavant24 <84028633+jabthesavant24@users.noreply.github.com>
2021-07-27 17:44:42 +0900

    Add files via upload

---
+++ FunctionParamExample.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+
+def minusNumber(x,y):
+    print(x-y)
+
+def multipleNumber(x,y):
+    print(x*y)
+
+addNumber(100,25)
+minusNumber(100,25)
+multipleNumber(100,25)
